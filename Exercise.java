6be843e1c193fbf977438a8c7fd434ee152039dc
Sebastian Geimer <sebastain.geimer@web.de>
2024-01-18 16:56:59 +0100

    dataObjects01

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,13 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+    // Erstelle eine ausführbare Klasse, welche die drei Variablen name (Datentyp String), age (Datentyp int) und gender (Datentyp char) deklariert, initialisiert und über die Konsole ausgibt.
+    String name = "Sebastian";
+    int age = 19;
+    char gender = 'm';
+
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(gender);
   }
 }
