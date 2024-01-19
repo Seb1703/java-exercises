6e0b0c1b60a105c2db7131d8770d95e6e609d197
Sebastian Geimer <sebastain.geimer@web.de>
2024-01-19 14:08:01 +0100

    operators1

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,21 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+    // Erstelle eine ausführbare Klasse, welche den ganzzahligen Quotienten sowie den Divisionsrest einer Division auf der Konsole ausgibt.
+    int dividend = 5;
+    int divisor = 3;
+    int quotient = Exercise.getQuotient(dividend, divisor);
+    int rest = Exercise.getRest(dividend, divisor);
+
+    System.out.println("Quotient: " + quotient);
+    System.out.println("Rest: " + rest);
+  }
+
+  public static int getQuotient(int dividend, int divisor){
+    return dividend/divisor;
+  }
+
+  public static int getRest(int dividend, int divisor){
+    return dividend % divisor;
   }
 }
