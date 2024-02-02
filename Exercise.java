30e9be6e6d77580debcdf288502ae10fdd9cb999
Sebastian Geimer <sebastain.geimer@web.de>
2024-02-02 14:54:21 +0100

    geradezahl

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,16 @@
-public class Exercise {
+import java.util.Scanner;
 
-  public static void main(String[] args) {
-    // implement exercise here
-  }
+public class Exercise{
+
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Gib eine Ganzzahl ein: ");
+		int number = sc.nextInt();
+		
+		if(number%2 == 0){
+			System.out.println( number + " ist gerade!");
+		}else{
+			System.out.println( number + " ist ungerade!");
+		}
+	}
 }
