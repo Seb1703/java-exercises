9d2ff8414f0e330f41cea1f25c0937429fcde080
Sebastian Geimer <sebastain.geimer@web.de>
2024-02-02 14:46:12 +0100

    exer

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,17 @@
+import java.util.Scanner;
+
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+    Scanner sc = new Scanner(System.in);
+		System.out.println("Gebe eine Ganzzahl ein: ");
+		double num1 = sc.nextDouble();
+	
+		System.out.println("Gebe eine weitere Ganzzahl ein: ");
+		double num2 = sc.nextDouble();
+
+		double result = num1/num2;
+		System.out.println("Der Anteil deiner ersten Zahl an der zweiten beträgt: ");
+		System.out.println(result*100 + "%");
   }
 }
