1a171c813dc02f2e8f035baeda965260fc4f5abd
Sebastian Geimer <sebastain.geimer@web.de>
2024-02-02 15:08:56 +0100

    tr

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,33 @@
-public class Exercise {
+import java.util.Scanner;
 
-  public static void main(String[] args) {
-    // implement exercise here
-  }
+public class Exercise{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Gib den ersten Operanden ein: ");
+		double operand1 = sc.nextDouble();
+		System.out.print("Gib den Operator ein: ");
+    sc.nextLine();
+		String operator = sc.nextLine();
+		System.out.print("Gib den zweiten Operanden ein: ");
+		double operand2 = sc.nextDouble();
+		
+		double result;
+		switch(operator){
+			case "+":
+				result = operand1 + operand2;
+				break;
+			case "-":
+				result = operand1 - operand2;
+				break;
+			case "*":
+				result = operand1 * operand2;
+				break;
+			case "/":
+				result = operand1 / operand2;
+				break;
+      default:
+        result = 0;
+		}
+	  System.out.println("Das Ergebnis ist: " + result);
+	}
 }
