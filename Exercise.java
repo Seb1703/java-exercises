6a4b0efc8b69fa8f5545538ee3e0485d163b145e
Sebastian Geimer <sebastain.geimer@web.de>
2024-02-05 19:11:50 +0100

    exer

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,31 @@
-public class Exercise {
+import java.util.Scanner;
 
-  public static void main(String[] args) {
-    // implement exercise here
-  }
+public class Exercise{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Gib deinen Vorname ein: ");
+		String vorname = sc.nextLine();
+		System.out.print("Gib deinen Nachname ein: ");
+		String nachname = sc.nextLine();
+		System.out.print("Gib dein Geschlecht ein (m,w,d): ");
+		String geschlecht = sc.nextLine();
+		System.out.print("Gib dein Alter ein: ");
+		int alter = sc.nextInt();
+		
+		if(alter >= 18){
+			switch(geschlecht){
+				case "m":
+					System.out.println("Hallo Herr " + nachname);
+					break;
+				case "w":
+					System.out.println("Hallo Frau " + nachname);
+					break;
+				default:
+					System.out.println("Hallo " + vorname);
+					break;
+			}
+		}else{
+			System.out.println("Hallo " + vorname);
+		}
+	}
 }
