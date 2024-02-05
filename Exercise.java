9a2dcdb9f22a0b3c3f487298891e44c53beb29ed
Sebastian Geimer <sebastain.geimer@web.de>
2024-02-05 19:30:26 +0100

    ex

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,9 @@
-public class Exercise {
-
-  public static void main(String[] args) {
-    // implement exercise here
-  }
+public class Exercise{
+	public static void main(String[] args){
+		for(int i = 1; i < 11; i++){
+			for(int j = 1; j < 11; j++){
+				System.out.println(i*j);
+			}
+		}
+	}
 }
