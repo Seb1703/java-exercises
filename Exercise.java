623f03c8eee8c8ca9968026e09d390552d483db0
Sebastian Geimer <sebastain.geimer@web.de>
2024-02-05 16:57:42 +0100

    edx

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,25 @@
 public class Exercise {
-
   public static void main(String[] args) {
-    // implement exercise here
+    int[] one = { 1, 2, 3, 4 };
+    int[] two = { 5, 6, 7, 8 };
+		
+		int[] neu = Exercise.merge(one, two);
+		for(int number : neu){
+			System.out.println(number);
+		}
+  }
+  public static int[] merge(int[] first, int[] second) {
+    int laenge = first.length + second.length;
+		int[] neueListe = new int[laenge];
+		int i = 0;
+		for(int number : first){
+			neueListe[i] = number;
+			i++;
+		}
+		for(int number : second){
+			neueListe[i] = number;
+			i++;
+		}
+		return neueListe;
   }
 }
