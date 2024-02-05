305d59370100e2495301f771469258f1f9b2b4c0
Sebastian Geimer <sebastain.geimer@web.de>
2024-02-05 19:36:46 +0100

    primzahl

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,16 @@
-public class Exercise {
-
-  public static void main(String[] args) {
-    // implement exercise here
-  }
+public class Exercise{
+	public static void main(String[] args){
+		int number = 14;
+    boolean check = true;
+		for(int i = 2; i < number; i++){
+			if(number % i == 0){
+				System.out.println("Deine Zahl ist keine Primzahl!");
+        check = false;
+				break;
+			}
+		}
+    if(check){
+      System.out.println("Deine Zahl ist Primzahl!");
+    }
+	}
 }
