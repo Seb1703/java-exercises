4379f8a537616c5769431ceec7962e437f443171
Sebastian Geimer <sebastain.geimer@web.de>
2024-02-06 20:39:26 +0100

    loop8

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,33 @@
-public class Exercise {
+import java.util.Scanner;
 
-  public static void main(String[] args) {
-    // implement exercise here
-  }
+public class Exercise{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		boolean repeat;
+		double result;
+		do{
+			System.out.println("Gib bitte das Startkapital ein (in Euro): ");
+			double k0 = sc.nextDouble();
+			System.out.println("Gib bitte den Prozentsatz ein: ");
+			double p = sc.nextDouble();
+			p = p / 100;
+			System.out.println("Gib bitte die Anzahl Jahre ein: ");
+			int n = sc.nextInt();
+		
+			result = Exercise.calc(k0, p, n);
+
+			System.out.println("Ergebnis: Das Endkapital beträgt: " + result + " Euro");
+			System.out.println("Willst Du eine weitere Zinsrechnung durchführen (true, false)?: ");
+			repeat = sc.nextBoolean();
+		}while(repeat);
+		
+	}
+	public static double calc(double k0, double p, int n){
+		double result = k0;
+		for(int i = 0; i < n; i++){
+			result *= (1+p);
+		}
+		return result;
+	}
 }
