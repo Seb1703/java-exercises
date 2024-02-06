a66f7b775a69bb53b9670cc9385ba86c0ce6dcc1
Sebastian Geimer <sebastain.geimer@web.de>
2024-02-06 17:30:28 +0100

    loop5

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,26 @@
-public class Exercise {
+import java.util.Random;
+import java.util.Scanner;
 
-  public static void main(String[] args) {
-    // implement exercise here
-  }
+public class Exercise{
+	public static void main(String[] args){
+		Random random = new Random();
+		int randInt = random.nextInt(100)+1;
+		
+		Scanner sc = new Scanner(System.in);
+		int correct = 0;
+
+		int eingabe;
+		while(correct == 0){
+			System.out.println("Gib eine Zahl ein: ");
+			eingabe = sc.nextInt();
+			if(eingabe == randInt){
+				correct = 1;
+				System.out.println("Du hast richtig geraten!");
+			}else{
+				System.out.println("Leider falsch!");
+				System.out.println("Weiter Spielen? Gib 0 ein, sonst 1!");
+				correct = sc.nextInt();
+			}
+		}
+	}
 }
