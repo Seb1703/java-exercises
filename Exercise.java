d7b86d0293effc4aaa5179b671268b554a033963
Sebastian Geimer <sebastain.geimer@web.de>
2024-02-07 08:53:19 +0100

    exercise

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,15 @@
-public class Exercise {
+public class Exercise{
+	public static void main(String[] args){
+		int[] myArray = {1, 2, 3, 4, 5};
+		
+		int[] switchedArray = new int[myArray.length];
 
-  public static void main(String[] args) {
-    // implement exercise here
-  }
+		for(int i = 0; i < myArray.length; i++){
+			switchedArray[i] = myArray[myArray.length - 1 - i];
+		}
+
+    for(int element : switchedArray){
+      System.out.println(element);
+    }
+	}
 }
