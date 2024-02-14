68dbc87ab4fd9bd7280ebddbaeee2a250323d022
Sebastian Geimer <sebastain.geimer@web.de>
2024-02-14 18:35:43 +0100

    exre

---
+++ Creature.java
@@ -0,0 +1,15 @@
+public class Creature {
+    private String name;
+    private int attackValue;
+    private int hitPoints;
+
+    public void Creature(String name, int attackValue, int hitPoints){
+        this.name = name;
+        this.attackValue = attackValue;
+        this.hitPoints = hitPoints;
+    }
+
+    public void attackCreature(Creature enemy){
+        this.attackValue
+    }
+}
