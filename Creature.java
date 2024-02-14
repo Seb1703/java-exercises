958ff674cc6447f0a57b321eb126f9c14541854b
Sebastian Geimer <sebastain.geimer@web.de>
2024-02-14 18:38:28 +0100

    solutions

--- Creature.java
+++ Creature.java
@@ -1,15 +1,37 @@
 public class Creature {
+
     private String name;
     private int attackValue;
-    private int hitPoints;
-
-    public void Creature(String name, int attackValue, int hitPoints){
-        this.name = name;
-        this.attackValue = attackValue;
-        this.hitPoints = hitPoints;
+    private int hitpoints;
+  
+    public Creature(String name, int attackValue, int hitpoints) {
+      this.name = name;
+      this.attackValue = attackValue;
+      this.hitpoints = hitpoints;
     }
-
-    public void attackCreature(Creature enemy){
-        this.attackValue
+  
+    public String getName() {
+      return name;
     }
-}
+  
+    public int getAttackValue() {
+      return attackValue;
+    }
+  
+    public int getHitpoints() {
+      return hitpoints;
+    }
+  
+    public void attackCreature(Creature creature) {
+      creature.hitpoints -= this.attackValue;
+      System.out.println(
+          this.name
+              + " greift "
+              + creature.name
+              + " an und erzielt "
+              + this.attackValue
+              + " Schaden");
+      System.out.println(creature.name + " hat noch " + creature.hitpoints + " Lebenspunkte");
+    }
+  }
+  
