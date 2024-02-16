5639a943a1ac4e2d4c170543e8743982162b7ee3
Sebastian Geimer <sebastain.geimer@web.de>
2024-02-16 15:20:22 +0100

    exercise

--- Exercise.java
+++ Exercise.java
@@ -1,6 +1,24 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    // implement exercise here
+    Dice dice1 = new Dice(1);
+    Dice dice2 = new Dice(2);
+
+    Player player1 = new Player("Lisa");
+    Player player2 = new Player("Hans");
+
+    player1.setScore(dice1.getValue());
+    player2.setScore(dice2.getValue());
+
+    System.out.println(player1.name() + " würfelt eine " + player1.getScore());
+    System.out.println(player2.name() + " würfelt eine " + player2.getScore());
+
+    if(player1.getScore() < player2.getScore()){
+      System.out.println(player2.name() + " hat gewonnen!");
+    }else if(player1.getScore() > player2.getScore()){
+      System.out.println(player1.name() + " hat gewonnen!");
+    }else{
+      System.out.println("Unentschieden!");
+    } 
   }
 }
---
+++ Player.java
@@ -0,0 +1,20 @@
+public class Player {
+    private String name;
+    private int score;
+
+    public Player(String name){
+        this.name = name;
+    }
+
+    public String name(){
+        return name;
+    }
+
+    public int getScore(){
+        return score;
+    }
+
+    public void setScore(int score){
+        this.score = score;
+    }
+}
