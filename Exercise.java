49da925e99e8a214b3e1e8150e6ef37c237881d5
Sebastian Geimer <sebastain.geimer@web.de>
2024-02-21 17:40:46 +0100

    first try

--- Exercise.java
+++ Exercise.java
@@ -1,24 +1,10 @@
 public class Exercise {
 
   public static void main(String[] args) {
-    int numberOfVehicles;
-    numberOfVehicles = Vehicle.getNumberOfVehicles();
-    System.out.println("Anzahl Fahrzeuge: " + numberOfVehicles);
+    Rental mueller = new Rental("Müller");
 
-    Car car1 = new Car("Porsche", "911", Engine.ELECTRO, 2);
-    Truck truck1 = new Truck("MAN", "TGX", Engine.DIESEL, 20);
-    Car car2 = new Car("Opel", "Zafira Life", Engine.DIESEL, 7);
-
-    numberOfVehicles = Vehicle.getNumberOfVehicles();
-    System.out.println("Anzahl Fahrzeuge: " + numberOfVehicles);
-
-    car1.print();
-    truck1.print();
-    car2.print();
-
-    car1.accelerate(50);
-    truck1.transform();
-    car1.doATurboBoost();
-    truck1.transform();
+    mueller.add(new Car("Porsche", "911", Engine.ELECTRO, 2));
+    mueller.add(new Truck("MAN", "TGX", Engine.DIESEL, 20));
+    mueller.add(new Car("Opel", "Zafira Life", Engine.DIESEL, 7));
   }
 }
---
+++ Rental.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+public class Rental {
+    private final String name;
+    private final ArrayList<Vehicle> vehicles;
+
+    public Rental(String name){
+        this.name = name;
+        vehicles = new ArrayList<>();
+    }
+
+    public String name(){
+        return name;
+    }
+
+    public ArrayList<Vehicle> vehicles(){
+        return vehicles;
+    }
+
+    public void addVehicle(Vehicle vehicle){
+        vehicles.add(vehicle);
+    }
+
+    public void addVehicles(ArrayList<Vehicle> vehicles){
+        this.vehicles.addAll(vehicles);
+    }
+
+    public void print(){
+        System.out.println("Fahrzeugvermietung " + name);
+        System.out.println("Unsere Fahrzeuge: ");
+        for(Vehicle vehicle : vehicles){
+            vehicle.print();
+        }
+    }
+}
