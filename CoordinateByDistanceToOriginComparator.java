c9b6dbb912c1ec4f73296373717abc1dbc1c2a3e
Sebastian Geimer <sebastain.geimer@web.de>
2024-03-01 16:58:10 +0100

    exercise

--- CoordinateByDistanceToOriginComparator.java
+++ CoordinateByDistanceToOriginComparator.java
@@ -1,8 +1,10 @@
+import java.util.Comparator;
+
 public class CoordinateByDistanceToOriginComparator implements Comparator<Coordinate> {
     public int compare(Coordinate c1, Coordinate c2){
         if(c1.getDistanceToOriginPoint() < c2.getDistanceToOriginPoint()){
             return -1;
-        }else if(c1.getDistanceToOriginPoint() > c2.getDistanceToOriginPoint())){
+        }else if(c1.getDistanceToOriginPoint() > c2.getDistanceToOriginPoint()){
             return 1;
         }else{
             return 0;
